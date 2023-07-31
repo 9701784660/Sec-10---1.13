0c7e9793cbd0fa1c05a69db6cd243e3f7858584e
praveen4858 <praveenryali048@gmail.com>
2023-07-31 16:47:52 +0530

    first commit

---
+++ IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec_10___1._13
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+
+}
